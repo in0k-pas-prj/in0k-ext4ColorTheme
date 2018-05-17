f65dffdf9d9c89bc5aad219a117d36ccad66e274
in0k <imp13.in0k@gmail.com>
2018-05-17 23:13:43 +0500

    generation of palette by systemColors

--- demo/Unit1.pas
+++ demo/Unit1.pas
@@ -14,13 +14,18 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    ColorBox1: TColorBox;
     ColorListBox1: TColorListBox;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
     procedure ColorListBox1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
   private
-    { private declarations }
+    procedure Colors2ColorListBox(const Colors:tExt4ColorTheme_Colors; const ColorListBox:TColorListBox);
   public
     { public declarations }
     colors:tExt4ColorTheme_Colors;
@@ -39,46 +44,46 @@ procedure TForm1.FormCreate(Sender:TObject);
 begin
     Ext4ColorTheme_Colors__INI(colors);
     //
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clHighlight));
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clHighlightedText));
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clHighlightText));
+    {Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clHighlight));
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clHighlightedText));
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clHighlightText));
 
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clHotLight));
-
-
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clBackground              ));
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clActiveCaption           ));
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clInactiveCaption         ));
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clMenu                    ));//: Ne));    ));
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clWindow                  ));//: NewColorName := rsWindowColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clWindowFrame             ));//: NewColorName := rsWindowFrameColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clMenuText                ));//: NewColorName := rsMenuTextColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clWindowText              ));//: NewColorName := rsWindowTextColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clCaptionText             ));//: NewColorName := rsCaptionTextColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clActiveBorder            ));//: NewColorName := rsActiveBorderColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clInactiveBorder          ));//: NewColorName := rsInactiveBorderColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clAppWorkspace            ));//: NewColorName := rsAppWorkspaceColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clHighlight               ));//: NewColorName := rsHighlightColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clHighlightText           ));//: NewColorName := rsHighlightTextColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clBtnFace                 ));//: NewColorName := rsBtnFaceColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clBtnShadow               ));//: NewColorName := rsBtnShadowColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clGrayText                ));//: NewColorName := rsGrayTextColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clBtnText                 ));//: NewColorName := rsBtnTextColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clInactiveCaptionText     ));//: NewColorName := rsInactiveCaptionText;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clBtnHighlight            ));//: NewColorName := rsBtnHighlightColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(cl3DDkShadow              ));//: NewColorName := rs3DDkShadowColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(cl3DLight                 ));//: NewColorName := rs3DLightColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clInfoText                ));//: NewColorName := rsInfoTextColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clInfoBk                  ));//: NewColorName := rsInfoBkColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clHotLight                ));//: NewColorName := rsHotLightColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clGradientActiveCaption   ));//: NewColorName := rsGradientActiveCaptionColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clGradientInactiveCaption ));//: NewColorName := rsGradientInactiveCaptionColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clMenuHighlight           ));//: NewColorName := rsMenuHighlightColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clMenuBar                 ));//: NewColorName := rsMenuBarColorCaption;
-    Ext4ColorTheme_Colors__ADD(colors,ColorToRGB(clForm                    ));//: NewColorName := rsFormColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clHotLight));
 
 
-
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clBackground              ));
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clActiveCaption           ));
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clInactiveCaption         ));
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clMenu                    ));//: Ne));    ));
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clWindow                  ));//: NewColorName := rsWindowColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clWindowFrame             ));//: NewColorName := rsWindowFrameColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clMenuText                ));//: NewColorName := rsMenuTextColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clWindowText              ));//: NewColorName := rsWindowTextColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clCaptionText             ));//: NewColorName := rsCaptionTextColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clActiveBorder            ));//: NewColorName := rsActiveBorderColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clInactiveBorder          ));//: NewColorName := rsInactiveBorderColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clAppWorkspace            ));//: NewColorName := rsAppWorkspaceColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clHighlight               ));//: NewColorName := rsHighlightColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clHighlightText           ));//: NewColorName := rsHighlightTextColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clBtnFace                 ));//: NewColorName := rsBtnFaceColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clBtnShadow               ));//: NewColorName := rsBtnShadowColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clGrayText                ));//: NewColorName := rsGrayTextColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clBtnText                 ));//: NewColorName := rsBtnTextColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clInactiveCaptionText     ));//: NewColorName := rsInactiveCaptionText;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clBtnHighlight            ));//: NewColorName := rsBtnHighlightColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(cl3DDkShadow              ));//: NewColorName := rs3DDkShadowColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(cl3DLight                 ));//: NewColorName := rs3DLightColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clInfoText                ));//: NewColorName := rsInfoTextColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clInfoBk                  ));//: NewColorName := rsInfoBkColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clHotLight                ));//: NewColorName := rsHotLightColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clGradientActiveCaption   ));//: NewColorName := rsGradientActiveCaptionColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clGradientInactiveCaption ));//: NewColorName := rsGradientInactiveCaptionColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clMenuHighlight           ));//: NewColorName := rsMenuHighlightColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clMenuBar                 ));//: NewColorName := rsMenuBarColorCaption;
+    Ext4ColorTheme_Colors__addProduce(colors,ColorToRGB(clForm                    ));//: NewColorName := rsFormColorCaption;
+    }
+    Ext4ColorTheme_Colors__addProduce_sysColors(colors);
+    Colors2ColorListBox(colors,ColorListBox1);
    // Screen.f;
 end;
 
@@ -86,6 +91,25 @@ procedure TForm1.ColorListBox1Click(Sender: TObject);
 begin
 
 end;
+
+procedure TForm1.Colors2ColorListBox(const Colors:tExt4ColorTheme_Colors; const ColorListBox:TColorListBox);
+var tmp:pointer;
+    clr:tColor;
+begin
+    ColorListBox.Items.BeginUpdate;
+    //
+    ColorListBox.Clear;
+    tmp:=Ext4ColorTheme_Colors__enumFirst(colors);
+    while Assigned(tmp) do begin
+        clr:=Ext4ColorTheme_Colors__enumColor(tmp);
+        ColorListBox.Items.AddObject('#'+IntToHex(clr,6)+' '+FloatToStr(Ext4ColorTheme_Colors__enumDelta(tmp)),tObject(clr));
+        tmp:=Ext4ColorTheme_Colors__enumNext(tmp);
+    end;
+    //
+    ColorListBox.Items.EndUpdate;
+    Caption:=inttostr(ColorListBox1.Items.Count);
+end;
+
 
 procedure TForm1.Button1Click(Sender: TObject);
 var tmp:pointer;
@@ -101,6 +125,18 @@ begin
     end;
 
     Caption:=inttostr(ColorListBox1.Items.Count);
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+    Ext4ColorTheme_Colors__delSimilar_sysColors(colors);
+    Colors2ColorListBox(colors,ColorListBox1);
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+    Ext4ColorTheme_Colors__sort(colors, ColorToRGB(ColorBox1.Selected));
+    Colors2ColorListBox(colors,ColorListBox1);
 end;
 
 procedure TForm1.FormDestroy(Sender: TObject);
